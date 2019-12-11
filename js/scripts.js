cd86b4192f1e03c010319e5901d7345ae28f527c
Kristian Suhartono <blackxephos2@gmail.com>
2019-12-11 23:50:53 +0800

    added google sheet access

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,61 @@
-$(function() {
+$( document ).ready(function() {
+  var apiKey = "AIzaSyB-iQu9eZoF5irnM2WijDrd_38rE033Yes";
+  var spreadsheetId = "172Hm8BIEoSPwneB1rpaeMJLM0q4GsR9XAXwe1aMVZ78";
+  
+  const Http = new XMLHttpRequest();
+  const url="https://sheets.googleapis.com/v4/spreadsheets/" +
+    spreadsheetId + "/values/Sheet1!A2:B10" + "?key="+apiKey+"&majorDimension=COLUMNS";
+  
+  Http.open("GET", url);
+  Http.send();
 
-    $(".progress").each(function() {
+  Http.onreadystatechange = (e) => {
+    if (Http.readyState == 4 && Http.status == 200) {
+      var parsed_data = JSON.parse(Http.responseText);
+      var max = parsed_data.values[0];
+      var values = parsed_data.values[1];
+      for(i=0; i < values.length; i++) {
+        barId = "bar" + i;
+        valueId = "value" + i;
+        var bar = document.getElementById(barId);
+        var display = document.getElementById(valueId);
+
+        dataVal = parseInt(values[i])
+        dataPercentage = dataVal * 100 / parseFloat(max[i])
+
+        bar.setAttribute("data-value", dataPercentage)
+        display.innerHTML = dataVal
+      };
+
+      $(".progress").each(function() {
   
-      var value = $(this).attr('data-value');
-      var left = $(this).find('.progress-left .progress-bar');
-      var right = $(this).find('.progress-right .progress-bar');
-  
-      if (value > 0) {
-        if (value <= 50) {
-          right.css('transform', 'rotate(' + percentageToDegrees(value) + 'deg)')
-        } else {
-          right.css('transform', 'rotate(180deg)')
-          left.css('transform', 'rotate(' + percentageToDegrees(value - 50) + 'deg)')
+        var value = $(this).attr('data-value');
+        var left = $(this).find('.progress-left .progress-bar');
+        var right = $(this).find('.progress-right .progress-bar');
+    
+        if (value > 0) {
+          if (value <= 50) {
+            right.css('transform', 'rotate(' + percentageToDegrees(value) + 'deg)')
+          } else {
+            right.css('transform', 'rotate(180deg)')
+            left.css('transform', 'rotate(' + percentageToDegrees(value - 50) + 'deg)')
+          }
         }
+    
+      })
+    
+      function percentageToDegrees(percentage) {
+    
+        return percentage / 100 * 360
+    
       }
-  
-    })
-  
-    function percentageToDegrees(percentage) {
-  
-      return percentage / 100 * 360
-  
     }
+  }
+
   
-  });
+});
   
+  function googleApiCall() {
+    
+  }
+
